8596aa96a96d19b5a86cee8380f1bd84fae0cf25
LucreR2003 <lucrerios95@gmail.com>
2022-08-19 19:14:22 -0300

    agrego css y react del componente panels

--- src/components/Panels/index.jsx
+++ src/components/Panels/index.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import "./Panels.css";
 
-function Panels (){
-    React.useEffect(() =>{
+
+function Panels () {
+    const [totalUsers, setTotalUsers] = useState([])
+    const [totalProducts, setTotalProducts] = useState([])
+    useEffect(() =>{
         fetch("/api/users")
         .then(res => res.json())
         .then(data => {
-           
+            setTotalUsers(data.count)
+        })
+        fetch("/api/products?page=1")
+        .then(res => res.json())
+        .then(data => {
+            setTotalProducts(data)
         })
     }, [])
     return (
-        <div>
-            <h3>Total de productos</h3>
-            <h3>Total de usuarios </h3>
-            <h3>Total de categorias</h3>
+        <div className='panels-container'>
+            <h3>Total de productos {totalProducts.count}</h3>
+            <h3>Total de usuarios {totalUsers}</h3>
+            <h3>Total de categorias {totalProducts.categories ? totalProducts.categories.length : ""} </h3>
         </div>
     )
 }
