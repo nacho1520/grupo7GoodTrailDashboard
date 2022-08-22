2dfaea5c4bb34443f2b29cdcfb10c5afe560134c
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-22 19:21:48 -0300

    Componente Loader

---
+++ src/components/LoaderSpinner/LoaderSpinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './LoaderSpinner.css';
+
+const LoaderSpinner = () => {
+    return(
+        <div className="spinner"></div>
+    );
+}
+
+export default LoaderSpinner;
