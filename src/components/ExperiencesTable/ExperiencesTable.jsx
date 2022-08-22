887a0b484ebc8112e9feb86d9b24df85886552c9
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-22 19:41:59 -0300

    Agrego el id y la categoria a la tabla de experiencias

--- src/components/ExperiencesTable/ExperiencesTable.jsx
+++ src/components/ExperiencesTable/ExperiencesTable.jsx
@@ -11,16 +11,20 @@ const ExperiencesTable = () => {
             <table className="table">
                 <thead>
                     <tr>
+                        <th>ID</th>
                         <th>Nombre</th>
                         <th>Descripción</th>
+                        <th>Categoria</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data && data.length > 0  && data.map((exp, index) => {
                         return(
                             <tr key={index}>
+                                <td>{exp.id}</td>
                                 <td>{exp.name}</td>
                                 <td>{exp.description}</td>
+                                <td>{exp.category.category_name}</td>
                             </tr>
                         );}) 
                     }
