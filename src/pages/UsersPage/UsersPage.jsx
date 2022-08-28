36a1ec85162a06201f11c4b9d7ad1144a3a47f95
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-28 15:19:32 -0300

    Mejoro los estilos de las paginas de Experiencias y usuarios

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import UsersTable from '../../components/UsersTable/UsersTable';
+import './UsersPage.css';
 
 const UsersPage = () => {
     return(
         <div className="experience-container">
             <div className="title-container">
-                <h1 className="experiences-title">Experiencias</h1>
+                <h1 className="experiences-title">Usuarios</h1>
             </div>
             <UsersTable />
         </div>
